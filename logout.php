458fa6b19bc4ec69e59f7d2f966f61d5b17eb171
Kyle Johnson <kylejohnson2612@gmail.com>
2017-10-25 17:13:15 -0600

    Fix logout bug for mem_id

--- logout.php
+++ logout.php
@@ -2,7 +2,7 @@
 
 setcookie('username', '', time()-3600);
 setcookie('name', '', time()-3600);
-setcookie('id', '', time()-3600);
+setcookie('id', 0, time() + (60*60*24*30));
 header('Location: index.php');
 
  ?>
--- nav.php
+++ nav.php
@@ -5,17 +5,17 @@
   <li><a href="products.php"><i class="fa fa-automobile"></i> Products</a></li>
   <li><a href="email.php"><i class="fa fa-bell"></i> Contact</a></li>
   <li><a href="cart.php"><i class="fa fa-shopping-cart"></i> Cart (<?php 
+  require_once('variable.php');
+  $dbconnect = mysqli_connect(HOST, USERNAME, PASSWORD, DATABASE) or die('connection failed');
+  $queryCheck = "SELECT * FROM cart";
+  $resultCheck = mysqli_query($dbconnect, $queryCheck) or die('cart query failed');
+  $cartCount = 0;
   if(isset($_COOKIE["id"])){
     $mem_id = $_COOKIE["id"];
   }else{
     setcookie('id', 0, time() + (60*60*24*30));
     $mem_id = $_COOKIE["id"];
   };
-  require_once('variable.php');
-  $dbconnect = mysqli_connect(HOST, USERNAME, PASSWORD, DATABASE) or die('connection failed');
-  $queryCheck = "SELECT * FROM cart";
-  $resultCheck = mysqli_query($dbconnect, $queryCheck) or die('cart query failed');
-  $cartCount = 0;
   while($rowCheck = mysqli_fetch_array($resultCheck)){
     if($rowCheck['mem_id'] == $mem_id){
       $cartCount += 1;
