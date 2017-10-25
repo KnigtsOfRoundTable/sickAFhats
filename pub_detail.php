07c43b037dd86d197d88c742bc0aa9e9065f3989
Kyle Johnson <kylejohnson2612@gmail.com>
2017-10-25 16:55:05 -0600

    Fix bug that if item is already in cart, user can't add same item to cart

--- pub_detail.php
+++ pub_detail.php
@@ -2,21 +2,47 @@
 require_once('variable.php');
 
 $product_id = $_GET['id'];
-$mem_id = $_COOKIE['id'];
+if(isset($_COOKIE["id"])){
+  $mem_id = $_COOKIE["id"];
+}else{
+  setcookie('id', 0, time() + (60*60*24*30));
+  $mem_id = $_COOKIE["id"];
+};
+
 
 if(isset($_POST['submit'])){
     $product_id = $_POST[newProductID];
-  
-    $dbconnect2 = mysqli_connect(HOST, USERNAME, PASSWORD, DATABASE) or die('connection failed');
-  
-    $query2 = " INSERT INTO cart (product_id, mem_id) VALUES ('$product_id', '$mem_id')";
-  
-    $result2 = mysqli_query($dbconnect2, $query2) or die('run query failed');
+
+    $testing = false;
+
+    $dbconnect3 = mysqli_connect(HOST, USERNAME, PASSWORD, DATABASE) or die('connection failed');
     
-    mysqli_close($dbconnect2);
+    $query3 = "SELECT * FROM cart";
     
-    header('Location: cart.php');
+    $result3 = mysqli_query($dbconnect3, $query3) or die ('display query run failed');
+
+    while($row = mysqli_fetch_array($result3)){
+      $rowId = $row["mem_id"];
+      if($row['product_id'] == $product_id && $row['mem_id'] == $mem_id){
+        $testing = true;
+      };
+    };
+
+    if($testing == false){
+      $dbconnect2 = mysqli_connect(HOST, USERNAME, PASSWORD, DATABASE) or die('connection failed');
+      
+      $query2 = " INSERT INTO cart (product_id, mem_id) VALUES ('$product_id', '$mem_id')";
+    
+      $result2 = mysqli_query($dbconnect2, $query2) or die('run query failed');
+      
+      mysqli_close($dbconnect2);
+      
+      header('Location: cart.php');
+    }else{
+      header('Location: cart.php');
   
+    };
+    
   }else{
     $dbconnect = mysqli_connect(HOST, USERNAME, PASSWORD, DATABASE) or die('connection failed');
     
